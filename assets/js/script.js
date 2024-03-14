ea2b2fbf0f9c744193b833abcf3753a2572c6dba
fadill980 <akungithub878@gmail.com>
2024-03-14 22:54:48 +0700

    update 14/03/2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,18 @@ const ketik = new Typed(".typing", {
     showCursor: true,
     cursorChar: '|',
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
