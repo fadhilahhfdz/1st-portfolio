3a1f5cd23b2e4a6f7b5565331bc5e6ad73257e36
fadill980 <akungithub878@gmail.com>
2024-05-18 14:20:44 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
-
-var splide = new Splide( '.splide', {
-    type   : 'loop',
-    perPage: 3,
-    perMove: 1,
-  } );
-  
-  splide.mount();
